5a15d61b3421dff5aaabe3c9f68b144be5c083b4
Abhijeet Mourya <67868256+abhijeetmourya@users.noreply.github.com>
2021-08-23 21:55:47 +0530

    error fixed again!!!!!

---
+++ message-sender.php
@@ -0,0 +1,23 @@
+<?php 
+    if (isset($_POST['submit'])) {
+        $name = $_POST['name'];
+        $viewer_email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['msg'];
+
+        $sender_email = 'abhijeetmourya678@gmail.com';
+        $recciver_email = "abhijeetmourya789@gmail.com";
+        
+        $email_subject = "New Form Submission in Healic";
+        $body = "Name: $name. \n";
+                "Email: $viewer_email. \n";
+                "Subject: $subject. \n";
+                "Message: $message. \n";
+
+        $headers = "From: $sender_email \r\n";
+        $headers = "Reply-To: $viewer_email \r\n";
+
+        mail($recciver_email,$email_subject,$body,$headers)
+        header("Location: index.html?mailsend")
+    }
+?>
